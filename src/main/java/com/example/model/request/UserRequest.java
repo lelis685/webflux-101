566d5829c86e82cd1eef499f0c5a904ad2912348
lelis <lelis685@gmail.com>
2024-05-17 15:36:10 -0300

    add annotation trimString validation

--- src/main/java/com/example/model/request/UserRequest.java
+++ src/main/java/com/example/model/request/UserRequest.java
@@ -1,5 +1,6 @@
 package com.example.model.request;
 
+import com.example.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -7,11 +8,14 @@ import jakarta.validation.constraints.Size;
 public record UserRequest(
         @Size(min = 3, max = 50)
         @NotBlank
+        @TrimString
         String name,
         @Email
+        @TrimString
         String email,
         @Size(min = 3, max = 20)
         @NotBlank
+        @TrimString
         String password
 ) {
 
